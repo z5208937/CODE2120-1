3e9ecffa0c21578045547140981f7b6d12a0044e
Joshua Song <36873718+z5208937@users.noreply.github.com>
2019-06-06 12:05:42 +1000

    Week1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'example/', include('example.urls')),
+	path(r'Azure/', include('Azure.urls')),
 ]
